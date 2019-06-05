fd67c55edf647c87dd9022053d13890fb46d6ee0
Grigory Baluev <gr.baluev@gmail.com>
2019-06-05 21:28:10 +0300

    hatch and solid render added def create color added

--- sync.py
+++ sync.py
@@ -1,5 +1,8 @@
 import csv
 import json
+import re
+
+filepath = 'sisufile.json'
 
 
 def read_sisufile(filepath):
@@ -20,7 +23,7 @@ def read_sisufile_csv(filepath):
             'layer': [x['layer'], {
                 'color': create_color(x['color']),
                 'lineType': x['linetype'],
-                'lineWeight': float(x['lineWeight']),
+                'lineWeight': float(x['lineweight']),
             }],
             'code': x['code'],
             'properties': {
@@ -28,13 +31,43 @@ def read_sisufile_csv(filepath):
                 'patternBasePoint': [0, 0, 0]
             },
             'view': [
-            ],
+                {
+                    'layerSuffix': '_SOLID',
+                    'render': ['hatch', {
+                        'pattern': 'Solid',
+                        'scale': 1,
+                        'color': create_color(x['solid_color']),
+                        'lineWeight': 0.1
+                    }]
+                },
+                {
+                    'layerSuffix': '_HATCH',
+                    'render': ['hatch', {
+                        'pattern': x['PAT_name'],
+                        'scale': x['PAT_scale'],
+                        'color': create_color(x['PAT_color']),
+                        'lineWeight': float(x['PAT_lineweight'])
+                    }],
+                }
+             ],
             'options': {}
         }
 
+
     reader = csv.DictReader(open(filepath, 'r'))
+
     return [row(x) for x in reader]
 
 
 def create_color(value):
-    return [0, 0, 0]
+    if re.match(r'\w+', value):
+        return [0, 0, 0]
+    color_list = value.split(',')
+    return [int(channel) for channel in color_list]
+
+
+if __name__ == '__main__':
+    color_red = 'black 123'
+
+    print (create_color(color_red))
+    print(read_sisufile(filepath))
