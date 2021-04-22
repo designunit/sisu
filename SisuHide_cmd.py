92deb2f33a4d917a720d427dfb0c29aa11f75274
grigorybaluev <gr.baluev@gmail.com>
2021-04-22 18:18:30 +0300

    debug SisuHide and SisuShow

--- SisuHide_cmd.py
+++ SisuHide_cmd.py
@@ -1,7 +1,6 @@
 import Rhino
 import rhinoscriptsyntax as rs
-from rhinolib import get_sisufile
-from sisulib import get_related_layers
+from rhinolib import get_sisufile, get_sisu_layers
 
 __commandname__ = 'SisuHide'
 
@@ -12,12 +11,13 @@ def RunCommand( is_interactive ):
         print('Sisufile not configured')
         return Rhino.Commands.Result.Failure
 
-    layers = get_related_layers(config, derived_only=True)
+    layers = get_sisu_layers(config, derived_only=True)
     for layer in layers:
         rs.LayerVisible(layer, visible=False)
         parent = rs.ParentLayer(layer)
         rs.ExpandLayer(parent, False) # collapse parend layer
 
+    print('Layers successfully hidden!')
     return Rhino.Commands.Result.Success
 
 
